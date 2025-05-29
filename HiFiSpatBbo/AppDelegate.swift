97035cfc2b53e8e114664f3fb9927178ec255741
mumulin1117 <mumulin1117@163.com>
2025-05-29 10:21:12 +0800

    项目初始化

---
+++ HiFiSpatBbo/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  HiFiSpatBbo
+//
+//  Created by mumu on 2025/5/27.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+  
+
+}
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'HiFiSpatBbo' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for HiFiSpatBbo
+pod "IQKeyboardManager"
+pod "SwiftyStoreKit"
+pod "SVProgressHUD"
+
+
+end
